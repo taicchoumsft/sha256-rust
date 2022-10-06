c6966d31f3998d3fc9445783bc433471f2dbd51f
Tai Chou <chouta@gmail.com>
2022-10-06 11:09:28 -0700

    minor formatting

--- src/sha2.rs
+++ src/sha2.rs
@@ -36,18 +36,17 @@ impl Sha2 {
 	Self {
 	    h: [0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19],
 	    k: [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-			   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-			   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-			   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-			   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-			   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-			   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-			   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
 	}
     }
 
     pub fn algo(self, filename: &String) {
-
 	//Pre-processing (Padding):
 	match self.preprocess(filename) {
 	    Ok(s) => {
@@ -131,56 +130,56 @@ impl Sha2 {
 	    //println!("{:?}", w);
 
 	    // Initialize working variables to current hash value
-	    let [mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h] = self.h;
+	    {
+		let [mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h] = self.h;
 
-	    // Compression function main loop
-	    for i in 0..64 {
-		let s1 = right_rotate(e, 6)
-		    ^ right_rotate(e, 11)
-		    ^ right_rotate(e, 25);
+		// Compression function main loop
+		for i in 0..64 {
+		    let s1 = right_rotate(e, 6)
+			^ right_rotate(e, 11)
+			^ right_rotate(e, 25);
 
-		let ch = (e & f) ^ (!e & g);
+		    let ch = (e & f) ^ (!e & g);
 
-		let temp1: u32 = ((h as u64 +
-		    s1 as u64 +
-		    ch as u64 +
-		    self.k[i] as u64 +
-		    w[i] as u64) % 2u64.pow(32)) as u32;
+		    let temp1: u32 = ((h as u64 +
+				       s1 as u64 +
+				       ch as u64 +
+				       self.k[i] as u64 +
+				       w[i] as u64) % 2u64.pow(32)) as u32;
 
-		let s0 = right_rotate(a, 2)
-		    ^ right_rotate(a, 13)
-		    ^ right_rotate(a, 22);
+		    let s0 = right_rotate(a, 2)
+			^ right_rotate(a, 13)
+			^ right_rotate(a, 22);
 
-		let maj = (a & b) ^ (a & c) ^ (b & c);
+		    let maj = (a & b) ^ (a & c) ^ (b & c);
 
-		let temp2 = ((s0 as u64 + maj as u64) % 2u64.pow(32)) as u32;
+		    let temp2 = ((s0 as u64 + maj as u64) % 2u64.pow(32)) as u32;
 
-		h = g;
-		g = f;
-		f = e;
-		e = ((d as u64 + temp1 as u64) % 2u64.pow(32)) as u32;
-		d = c;
-		c = b;
-		b = a;
-		a = ((temp1 as u64 + temp2 as u64) % 2u64.pow(32)) as u32;
+		    h = g;
+		    g = f;
+		    f = e;
+		    e = ((d as u64 + temp1 as u64) % 2u64.pow(32)) as u32;
+		    d = c;
+		    c = b;
+		    b = a;
+		    a = ((temp1 as u64 + temp2 as u64) % 2u64.pow(32)) as u32;
+		}
+
+		//Add the compressed chunk to the current hash value:
+		self.h[0] = ((self.h[0] as u64 + a as u64) % 2u64.pow(32)) as u32;
+		self.h[1] = ((self.h[1] as u64 + b as u64) % 2u64.pow(32)) as u32;
+		self.h[2] = ((self.h[2] as u64 + c as u64) % 2u64.pow(32)) as u32;
+		self.h[3] = ((self.h[3] as u64 + d as u64) % 2u64.pow(32)) as u32;
+		self.h[4] = ((self.h[4] as u64 + e as u64) % 2u64.pow(32)) as u32;
+		self.h[5] = ((self.h[5] as u64 + f as u64) % 2u64.pow(32)) as u32;
+		self.h[6] = ((self.h[6] as u64 + g as u64) % 2u64.pow(32)) as u32;
+		self.h[7] = ((self.h[7] as u64 + h as u64) % 2u64.pow(32)) as u32;
 	    }
-
-	    //Add the compressed chunk to the current hash value:
-	    self.h[0] = ((self.h[0] as u64 + a as u64) % 2u64.pow(32)) as u32;
-	    self.h[1] = ((self.h[1] as u64 + b as u64) % 2u64.pow(32)) as u32;
-	    self.h[2] = ((self.h[2] as u64 + c as u64) % 2u64.pow(32)) as u32;
-	    self.h[3] = ((self.h[3] as u64 + d as u64) % 2u64.pow(32)) as u32;
-	    self.h[4] = ((self.h[4] as u64 + e as u64) % 2u64.pow(32)) as u32;
-	    self.h[5] = ((self.h[5] as u64 + f as u64) % 2u64.pow(32)) as u32;
-	    self.h[6] = ((self.h[6] as u64 + g as u64) % 2u64.pow(32)) as u32;
-	    self.h[7] = ((self.h[7] as u64 + h as u64) % 2u64.pow(32)) as u32;
 	}
 
 	let digest = self.h.iter()
 	    .map(|i| format!("{:x}", i))
 	    .collect::<String>();
-
-	//println!("{:?}", digest);
 
 	Ok(digest)
     }
