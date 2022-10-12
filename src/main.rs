32dd91b0220545d0c5075ce3968b8806ed3e0d86
Tai Chou <chouta@gmail.com>
2022-10-11 18:59:58 -0700

    Refactor, lint and format

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
+use sha2rust::Sha2;
 use std::env;
-
-pub mod sha2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let sha2obj = sha2::Sha2::new();
+    let sha2obj = Sha2::new();
 
     match args.len() {
-	2 => {
-	    sha2obj.algo(&args[1]);
-	}
-	_ => {
-	    println!("<filename> needed");
-	}
+        2 => match sha2obj.sum(&args[1]) {
+            Ok(s) => {
+                println!("{:?}", s);
+            }
+            Err(_) => {
+                eprintln!("Could not read file");
+            }
+        },
+        _ => {
+            println!("<filename> needed");
+        }
     };
 }
